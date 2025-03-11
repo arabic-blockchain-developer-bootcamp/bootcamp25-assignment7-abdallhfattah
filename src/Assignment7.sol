5a4e2f769e080a11ae0b158afc61e4ada667d4d7
abdallhfattah <abdallh.fattah33@gmail.com>
2025-03-11 17:21:23 +0200

    A7 [solved]

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -6,13 +6,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Assignment7 is ERC20 {
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+
+    }
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint() {
+    function mint(address address_, uint256 amount_) external {
         // Fill in the logic
         // call _mint function with to and amount params
+        super._mint(address_, amount_);
     }
 }
